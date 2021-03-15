d93f9fa7ee70b09b8a32596978cb1c69b2559be2
Eddie <dr.eduard.fadeev@gmail.com>
2021-03-15 11:08:01 +0100

    bam2fastx script

--- metaG/slurm_jobs/extract_fastq_from_bam.sh
+++ metaG/slurm_jobs/extract_fastq_from_bam.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=bam2fastq
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=3GB
+#SBATCH --mail-user=dr.eduard.fadeev@gmail.com
+#SBATCH --output=log/%x-%j.out
+#SBATCH --error=log/%x-%j.err
 
+#load module
+module load tophat/2.1.1
+
+#Set up the path to the working directory
+WORKDIR=/proj/DECOMB/data/process_metaG/
+cd $WORKDIR
+
+#run the program
+bam2fastx --fastq --all -o CDT3KANXX_2.fastq ./unzipped/CDT3KANXX_2\#0.bam
