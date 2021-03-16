55d77cc8729e0f72591f0248e7047166e2f923a5
Eduard Fadeev <dr.eduard.fadeev@gmail.com>
2021-03-16 13:38:23 +0100

    kraken2 corrected and assembly added

--- metaG/slurm_jobs/kraken2krona.sh
+++ metaG/slurm_jobs/kraken2krona.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 #
-#SBATCH --job-name=Kraken2_classificaion
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=48GB
+#SBATCH --job-name=Kraken2krona
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=3GB
 #SBATCH --mail-user=dr.eduard.fadeev@gmail.com
 #SBATCH --output=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.out
 #SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
 
 #load module
 module load kraken/2.0.8-beta
+module load krona/2.7
 
 #Set up the path to the working directory
 WORKDIR=/proj/DECOMB/analysis/process_metaG/
 cd $WORKDIR
-
-#run the program
-kraken2 --paired --use-names --db /mirror/kraken2/kraken_standard_db --threads 12 --output ./kraken2/CDT3KANXX_${SLURM_ARRAY_TASK_ID}.output --report ./kraken2/CDT3KANXX_${SLURM_ARRAY_TASK_ID}.report --classified-out ./kraken2/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_classified_#.fastq.gz ./TRIM/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R1.fastq.gz ./TRIM/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R2.fastq.gz
-
-#produce report in krona
-module load krona/2.7
 
 #define kraken tools directory
 KRAK_TOOLS_DIR=/proj/DECOMB/source/KrakenTools
