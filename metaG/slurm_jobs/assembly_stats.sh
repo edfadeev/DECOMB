288c8ba10effc55f69ef06671cfd21c50e879e18
Eddie <dr.eduard.fadeev@gmail.com>
2021-03-17 09:57:20 +0100

    QUAST stats addded

---
+++ metaG/slurm_jobs/assembly_stats.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=assembly_stats_Quast
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=20GB
+#SBATCH --mail-user=dr.eduard.fadeev@gmail.com
+#SBATCH --output=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.out
+#SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
+
+#load module
+module unload python3
+module load quast/5.0.2
+module load circos/0.69.9
+
+#Set up the path to the working directory
+WORKDIR=/proj/DECOMB/analysis/process_metaG/
+cd $WORKDIR
+
+#run QUAST
+metaquast.py ./assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/contigs.fasta \
+--threads 8 --circos --glimmer --rna-finding --test-no-ref \
+-1 ./assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/corrected/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R1.fastq.00.0_0.cor.fastq.gz \
+-2 ./assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/corrected/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R2.fastq.00.0_0.cor.fastq.gz \
+-o ./quast/CDT3KANXX_${SLURM_ARRAY_TASK_ID}
