741dcc234aeb6b2557a4833bf16ab81e85916c22
Eduard Fadeev <dr.eduard.fadeev@gmail.com>
2021-03-17 11:43:12 +0100

    quast script finalized

--- metaG/slurm_jobs/assembly_stats.sh
+++ metaG/slurm_jobs/assembly_stats.sh
@@ -8,20 +8,18 @@
 #SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
 
 #load module
-module load python3/3.7.0
+module load quast/5.0.2
 module load circos/0.69.9
+module load ncbiblastplus/2.10.0
 
 #Set up the path to the working directory
 WORKDIR=/proj/DECOMB/analysis/process_metaG/
 cd $WORKDIR
 
-#define kraken tools directory
-KRAK_TOOLS_DIR=/proj/DECOMB/source/KrakenTools
-
 #run QUAST
-metaquast.py ./assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/contigs.fasta \
+metaquast.py $WORKDIR/assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/contigs.fasta \
 --threads 8 --circos --glimmer --rna-finding \
---blast-db ./quast/silva_db/silva_db
--1 ./assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/corrected/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R1.fastq.00.0_0.cor.fastq.gz \
--2 ./assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/corrected/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R2.fastq.00.0_0.cor.fastq.gz \
--o ./quast/CDT3KANXX_${SLURM_ARRAY_TASK_ID}
+--blast-db $WORKDIR/quast/silva_db/silva_db \
+-1 $WORKDIR/assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/corrected/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R1.fastq.00.0_0.cor.fastq.gz \
+-2 $WORKDIR/assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/corrected/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R2.fastq.00.0_0.cor.fastq.gz \
+-o $WORKDIR/quast/CDT3KANXX_${SLURM_ARRAY_TASK_ID}
