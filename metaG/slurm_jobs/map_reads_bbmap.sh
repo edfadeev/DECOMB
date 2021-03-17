13871c17182528e2cb52866d404544ec40d99017
Eddie <dr.eduard.fadeev@gmail.com>
2021-03-17 17:24:05 +0100

    BBmap mapping added

---
+++ metaG/slurm_jobs/map_reads_bbmap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mapping-bbmap
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=20GB
+#SBATCH --mail-user=dr.eduard.fadeev@gmail.com
+#SBATCH --output=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.out
+#SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
+
+#load module
+module load bbmap/37.61
+
+#Set up the path to the working directory
+WORKDIR=/proj/DECOMB/analysis/process_metaG/
+cd $WORKDIR
+
+#run the program
+bbmap.sh in1=$WORKDIR/TRIM/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R1.fastq.gz \
+in2=$WORKDIR/TRIM/CDT3KANXX_${SLURM_ARRAY_TASK_ID}_QC_R2.fastq.gz \
+ref=$WORKDIR/assembly/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/scaffolds.fasta \
+out=$WORKDIR/mapping/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/scaffolds_mapped.sam \
+statsfile=$WORKDIR/mapping/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/scaffolds_mapped.stats \
+covstats=$WORKDIR/mapping/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/scaffolds_mapped.covstats \
+rpkm=$WORKDIR/mapping/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/scaffolds_mapped.rpkm \
+nodisk=t \
+Xmx=20G
