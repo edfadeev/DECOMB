4046521b82469c297d1aab78db686e3b74d3dc4d
Eduard Fadeev <dr.eduard.fadeev@gmail.com>
2021-03-22 10:55:30 +0100

    taxonomy corrected

--- metaG/slurm_jobs/anvio-add-taxonomy.sh
+++ metaG/slurm_jobs/anvio-add-taxonomy.sh
@@ -7,13 +7,19 @@
 #SBATCH --output=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.out
 #SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
 
+#load module
+module load diamond/0.9.36
 
 #Set up the path to the working directory
 WORKDIR=/proj/DECOMB/analysis/process_metaG/
 cd $WORKDIR
 
-#extimate taxonomy
+#identify single copy genes
+anvi-run-scg-taxonomy -c $WORKDIR/anvio/CDT3KANXX_${SLURM_ARRAY_TASK_ID}.db --num-threads 4 --num-parallel-processes 3
+
+
+#estimate taxonomy
 anvi-estimate-scg-taxonomy -c $WORKDIR/anvio/CDT3KANXX_${SLURM_ARRAY_TASK_ID}.db \
---num-parallel-processes 12 \
 -p $WORKDIR/anvio/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/PROFILE.db \
---num-threads 12 --metagenome-mode --compute-scg-coverages --update-profile-db-with-taxonomy
+--num-threads 12 --metagenome-mode --compute-scg-coverages --update-profile-db-with-taxonomy \
+--output-file $WORKDIR/anvio/CDT3KANXX_${SLURM_ARRAY_TASK_ID}/SCG_taxonomy.txt
