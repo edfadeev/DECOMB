69d40f6f56f6969c12f4f460a5ba7dbd6536367d
Eduard Fadeev <dr.eduard.fadeev@gmail.com>
2021-03-22 13:32:45 +0100

    assemblies currated

--- metaG/slurm_anvio/co_assembly_megahit.sh
+++ metaG/slurm_anvio/co_assembly_megahit.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=250GB
 #SBATCH --mail-user=dr.eduard.fadeev@gmail.com
-#SBATCH --output=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.out
-#SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
+#SBATCH --output=/proj/DECOMB/analysis/metaG_anvio/Log/%x-%j.out
+#SBATCH --error=/proj/DECOMB/analysis/metaG_anvio/Log/%x-%j.err
 
 #load module
 module load megahit/1.1.2
@@ -16,5 +16,5 @@ cd $WORKDIR
 
 #run the program
 megahit -1 $WORKDIR/01_QC/CDT3KANXX_105352_QC_R1.fastq.gz,$WORKDIR/01_QC/CDT3KANXX_105353_QC_R1.fastq.gz,$WORKDIR/01_QC/CDT3KANXX_105354_QC_R1.fastq.gz \
--2 $WORKDIR/01_QC/CDT3KANXX_105352_QC_R2.fastq.gz,$WORKDIR/01_QC/CDT3KANXX_105353_QC_R2.fastq.gz,$WORKDIR/01_QC/CDT3KANXX_105354_QC_R2.fastq.gz\
--o $WORKDIR/02_ASSEMBLY/megahit -t 20 --tmp-dir $TMPDIR/tmp
+-2 $WORKDIR/01_QC/CDT3KANXX_105352_QC_R2.fastq.gz,$WORKDIR/01_QC/CDT3KANXX_105353_QC_R2.fastq.gz,$WORKDIR/01_QC/CDT3KANXX_105354_QC_R2.fastq.gz \
+-o $WORKDIR/02_ASSEMBLY/megahit -t 20
