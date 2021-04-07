eb122dec63c6d3aaf77094f380fbae6ca884b804
Eduard Fadeev <dr.eduard.fadeev@gmail.com>
2021-04-07 21:00:24 +0200

    co assembly

--- metaG/slurm_anvio/co_assembly_spades.sh
+++ metaG/slurm_anvio/co_assembly_spades.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 #SBATCH --job-name=assembly_SPAdes
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=250GB
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=450GB
 #SBATCH --mail-user=dr.eduard.fadeev@gmail.com
-#SBATCH --output=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.out
-#SBATCH --error=/proj/DECOMB/analysis/process_metaG/Log/%x-%j.err
+#SBATCH --output=/proj/DECOMB/analysis/metaG_anvio/Log/%x-%j.out
+#SBATCH --error=/proj/DECOMB/analysis/metaG_anvio/Log/%x-%j.err
 
 #load module
 module load spades/3.14.1
@@ -15,12 +15,13 @@ WORKDIR=/proj/DECOMB/analysis/metaG_anvio
 cd $WORKDIR
 
 #merge fastq libraries
-cat $WORKDIR/01_QC/*_QC_R1.fastq.gz > $WORKDIR/01_QC/R1.fastq.gz
-cat $WORKDIR/01_QC/*_QC_R2.fastq.gz > $WORKDIR/01_QC/R2.fastq.gz
+#cat $WORKDIR/01_QC/*_QC_R1.fastq.gz > $WORKDIR/01_QC/R1.fastq.gz
+#cat $WORKDIR/01_QC/*_QC_R2.fastq.gz > $WORKDIR/01_QC/R2.fastq.gz
 
 #run the program
-spades.py --meta --tmp-dir $TMPDIR/tmp --threads 20 \
+spades.py --meta --tmp-dir $TMPDIR/tmp --threads 64 \
 -1 $WORKDIR/01_QC/R1.fastq.gz -2 $WORKDIR/01_QC/R2.fastq.gz \
--o $WORKDIR/02_ASSEMBLY/SPAdes
+--memory 450 \
+-o $WORKDIR/02_ASSEMBLY/SPAdes 
 
 mv $TMPDIR/tmp ./assembly/corrected/tmp
