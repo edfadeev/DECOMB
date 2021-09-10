59a78cbdadb9572996b61e2a761c195228557ac0
EddieFadeev <dr.eduard.fadeev@gmail.com>
2021-09-10 10:54:00 +0200

    pathway plotting placeholder added

--- scripts/temp_scripts/bin_met_exploration.R
+++ scripts/temp_scripts/bin_met_exploration.R
@@ -29,6 +29,84 @@ Bins.DEseq.enr.prot_by_module <- Bins.DEseq.enr.prot %>%
 
 
 
+
+
+
+#########################################################################
+#work in progress
+#########################################################################
+
+
+
+############################################################################
+#plot the pathways of interest for each bin
+############################################################################
+#pathways of interest
+pathways<- c("00010", #Glycolysis / Gluconeogenesis
+             "00020", #TCA cycle
+             "01200", #carbon metabolism
+             "00190", #Oxidative phosphorylation
+             "00071", #Fatty acid degradation
+             "00250", #Alanine, aspartate and glutamate metabolism
+             "00260", #Glycine, serine and threonine metabolism
+             "00270", #Cysteine and methionine metabolism
+             "00280", #Valine, leucine and isoleucine degradation
+             "00310", #Lysine degradation
+             "00330", #Arginine and proline metabolism
+             "00340", #Histidine metabolism
+             "00350", #Tyrosine metabolism
+             "00360", #Phenylalanine metabolism
+             "00380", #Tryptophan metabolism
+             "00400", #Phenylalanine, tyrosine and tryptophan biosynthesis
+             "00430",# Taurine and hypotaurine metabolism
+)
+
+
+
+#plot in a loop all the pathways of interest for each bin
+for (bin in unique(Bins.DEseq.enr.prot_by_module$genome_name)) {
+  Bins_gene_calls_KEGG_modules_complete<- Bins.DEseq.enr.prot_by_module %>%  
+    filter(genome_name == bin & module_completeness >0.7) 
+  
+  pathview(gene.data = Bins_gene_calls_KEGG_modules_complete$ko, 
+           pathway.id =pathways,
+           species = "ko", 
+           keys.align = "y", 
+           kegg.native = T, both.dirs = TRUE,
+           discrete	=list(gene=TRUE),
+           res = 300, cex = 0.25,
+           out.suffix = bin)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Bins.DEseq.enr.prot_by_module_bin<-Bins.DEseq.enr.prot_by_module %>% 
                       filter(genome_name == "Bin_115_2") %>% 
                         mutate(color = case_when(Type =="Jelly" ~ "red",
