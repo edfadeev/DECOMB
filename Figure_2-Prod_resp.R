5cfe499e0341025b2e34a4ac5327f272d59d94d4
Eddie Fadeev <dr.eduard.fadeev@gmail.com>
2023-01-16 16:12:42 +0100

    Figure updated according to new terminology

--- Figure_2-Prod_resp.R
+++ Figure_2-Prod_resp.R
@@ -10,44 +10,66 @@ fish_raw <- read.table("data/microscopy/FiSH_results.txt",
 ######################################
 # Plot productivity and respiration by taxa
 ######################################
-FiSH.p <- fish_raw %>% 
+FiSH.mean <- fish_raw %>% 
             select(-c(Abundance..cells.mL.1.)) %>% 
             reshape2::melt(id.vars =c("Time", "Treatment","Bottle")) %>%
             mutate(value = as.numeric(value),
                    Method = case_when(gsub("_.*","",variable)=="P" ~ "Production",
                                       gsub("_.*","",variable)=="R" ~ "Respiration"),
-                   Taxa = case_when(gsub("R_|P_","",variable)=="Bac" ~ "Bact.",
+                   Taxa = case_when(gsub("R_|P_","",variable)=="Bac" ~ "Total",
                                     gsub("R_|P_","",variable)=="Psu" ~ "Pseudo.",
                                     gsub("R_|P_","",variable)=="Vib" ~ "Vibrio.",
                                     gsub("R_|P_","",variable)=="Alt" ~ "Altero."),
-                   Time = factor(Time, levels= c("0","21","154"))) %>% 
+                   Time = factor(Time, levels= c("0","21","154")),
+                   Treatment = case_when(Treatment == "J" ~ "Cteno-OM",
+                                                Treatment == "C" ~ "Control")) %>% 
             filter(!is.na(value),
-                   #  !Time == "0"
+                   Time != "0"
                    ) %>% 
             group_by(Time, Treatment, Method,Taxa) %>%
             summarise(across(where(is.numeric), list(mean = mean, se = se))) %>% 
-            #ggplot(aes(x = Taxa, y = value_mean, fill = Taxa))+
-            ggplot(aes(x = Time, y = value_mean, colour = Treatment))+
-                #geom_bar(size = 3, stat = "identity")+
-                geom_point(size = 5, colour = "black")+
-                geom_point(size = 4)+
-                geom_errorbar(aes(ymin= value_mean-value_se, ymax= value_mean+value_se), width = 0.3)+
-                #facet_grid(Method~Time+Treatment)+
-                scale_colour_manual(values =c("Innoculum"="gray50",
-                                              "J"="red",
-                                              "C"="blue"))+
-                facet_grid(Method~Taxa)+
-                theme_bw()+
-                theme(panel.grid.major = element_blank(),
-                      panel.grid.minor = element_blank(),
-                      text = element_text(size = 20))
+  mutate(Taxa = factor(Taxa, levels= c("Total","Altero.","Pseudo.","Vibrio.")))
+            
+  
+#plot production  
+prod.p <-  FiSH.mean %>% 
+  filter(Method =="Production") %>% 
+  ggplot(aes(x = interaction(Taxa, Time), y = value_mean, colour = Taxa, group = Taxa))+
+  geom_errorbar(aes(ymin= value_mean-value_se, ymax= value_mean+value_se), width = 0.3)+
+  geom_point(size = 5, colour = "black")+
+  geom_point(size = 4)+
+  scale_colour_manual(values = tol21rainbow)+
+  facet_grid( ~ Treatment, scales = "free")+
+  theme_EF+
+  scale_y_continuous(labels = scales::scientific_format(digits = 0), n.breaks=4)+
+  theme(axis.text.x = element_text(angle = 90))
 
 #save the plot
-ggsave("./Figures/Figure_2-Prod_resp.pdf",
-       plot = FiSH.p,
-       units = "cm",
-       width = 30, height = 30, 
-       #scale = 1,
+ggsave("./Figures/Figure_2-Bac_prod.pdf",
+       plot = prod.p,
+       units = "mm",
+       width = 90, height = 90, 
+       scale = 2,
+       dpi = 300)
+
+res.p <-  FiSH.mean %>% 
+  filter(Method =="Respiration") %>% 
+  ggplot(aes(x = interaction(Taxa, Time), y = value_mean, colour = Taxa, group = Taxa))+
+  geom_point(size = 5, colour = "black")+
+  geom_point(size = 4)+
+  geom_errorbar(aes(ymin= value_mean-value_se, ymax= value_mean+value_se), width = 0.3)+
+  scale_colour_manual(values = tol21rainbow)+
+  facet_grid( ~ Treatment, scales = "free")+
+  theme_EF+
+  scale_y_continuous(labels = scales::scientific_format(digits = 0), n.breaks=4)+
+  theme(axis.text.x = element_text(angle = 90))
+
+#save the plot
+ggsave("./Figures/Figure_S2-Bac_res.pdf",
+       plot = res.p,
+       units = "mm",
+       width = 90, height = 90, 
+       scale = 2,
        dpi = 300)
 
 #print session info and clean the workspace
